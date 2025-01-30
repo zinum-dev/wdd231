500d7f744707b3cf5efd643fc7d9592a2c0dd606
zinum-dev <161540422+zinum-dev@users.noreply.github.com>
2025-01-30 08:33:37 -0300

    add css and animation, and thanks page

---
+++ chamber/scripts/thankyou.js
@@ -0,0 +1,39 @@
+const currentUrl = window.location.href;
+
+const formData = currentUrl.split('?')[1].split('&')
+
+const results = document.querySelector('#thanks');
+
+
+console.log(formData);
+results.innerHTML = `
+<p>Thank you, ${show("fname")} ${show("lname")}, for joining the St. George Chamber of Commerce!</p>
+            <p>We have received your application with the following details:</p>
+            <ul>
+                <li><strong>Email:</strong> ${show("email")}</li>
+                <li><strong>Mobile Phone Number:</strong> ${show("phone")}</li>
+                <li><strong>Business/Organization Name:</strong> ${show("business")}</li>
+                <li><strong>Submission Date:</strong> ${formatDateTime(show("timestamp"))}</li>
+            </ul>
+            <p>We will be in touch with you soon!</p>
+`
+
+function show(key) {
+    let result
+    formData.forEach((item) => {
+        if (item.startsWith(key)) {
+            result = item.split('=')[1].replace(/%40/g, '@').replace(/%2B/g, '+');
+        }
+    });
+    return (result);
+}
+
+function formatDateTime(dateString) {
+    let decodedDateString = decodeURIComponent(dateString);
+    let date = new Date(decodedDateString);
+    date = Intl.DateTimeFormat("en-US", {
+        dateStyle: "short",
+        timeStyle: "short"
+    }).format(date);
+    return date;
+}
